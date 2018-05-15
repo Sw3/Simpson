43f1f35d718b15a558b0143a835b43cf96692f52
Miguel <miguelgao@hotmail.com>
2018-05-15 00:54:40 -0500

    menu de navegacion iniciado

--- app/src/main/java/co/edu/uniquindio/android/electiva/proyecto/activity/Encargado/EncargadosActivity.kt
+++ app/src/main/java/co/edu/uniquindio/android/electiva/proyecto/activity/Encargado/EncargadosActivity.kt
@@ -1,24 +1,35 @@
 package co.edu.uniquindio.android.electiva.proyecto.activity.Encargado
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import co.edu.uniquindio.android.electiva.proyecto.Dao.Repository
 import co.edu.uniquindio.android.electiva.proyecto.R
+import co.edu.uniquindio.android.electiva.proyecto.activity.Cliente.ClientesActivity
+import co.edu.uniquindio.android.electiva.proyecto.fragment.Cliente.ListaDeClientesFragment
 import co.edu.uniquindio.android.electiva.proyecto.fragment.Encargado.DetalleDeEncargadoFragment
 import co.edu.uniquindio.android.electiva.proyecto.fragment.Encargado.ListaDeEncargadosFragment
 import co.edu.uniquindio.android.electiva.proyecto.vo.Encargado
-import java.util.*
+import kotlinx.android.synthetic.main.activity_cliente.*
+import kotlinx.android.synthetic.main.activity_encargado.*
 import kotlin.collections.ArrayList
 
 /**
  * Actividad principal de la aplicación, contiene un recycler view de Encargados
  * @author caflorezvi
  */
-class EncargadosActivity : AppCompatActivity(), ListaDeEncargadosFragment.OnEncargadoSeleccionadoListener {
+class EncargadosActivity() : AppCompatActivity(), ListaDeEncargadosFragment.OnEncargadoSeleccionadoListener, NavigationView.OnNavigationItemSelectedListener {
+
+
 
     lateinit var lista:ArrayList<Encargado>
     var fragment:Fragment? = null
@@ -42,6 +53,18 @@ class EncargadosActivity : AppCompatActivity(), ListaDeEncargadosFragment.OnEnca
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_encargado)
 
+        //navigation view
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        val toggle = ActionBarDrawerToggle(this, drawer_layout,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        navview.itemIconTintList = null
+        navview.setNavigationItemSelectedListener(this)
+
+        //finNavigation
         fragment = supportFragmentManager.findFragmentById(R.id.fragmentoDetalleEncargado)
 
         lista = Repository().ListEncargados()
@@ -61,7 +84,45 @@ class EncargadosActivity : AppCompatActivity(), ListaDeEncargadosFragment.OnEnca
         return true
     }
 
+    /**
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return super.onOptionsItemSelected(item)
     }
+**/
+    //metodos navigationView
+    fun reemplazarFragmento(act: Activity) {
+        var intent:Intent = Intent(this, act::class.java)
+        startActivity(intent)
+    }
+    fun mostrarMensaje(mensaje:String){
+        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_seccion_1 -> reemplazarFragmento(ClientesActivity())
+            R.id.menu_seccion_2 -> mostrarMensaje("Pulsada seccion 2")
+            R.id.menu_seccion_3 -> mostrarMensaje("Pulsada seccion 3")
+            R.id.menu_opcion_1 -> mostrarMensaje("Pulsada opción 1")
+
+        }
+        item.isChecked = true
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            android.R.id.home -> {
+                drawer_layout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+    //fin navview
 }
