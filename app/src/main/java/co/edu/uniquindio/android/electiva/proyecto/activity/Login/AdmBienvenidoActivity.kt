c40a9568b4348abc47b1a241b9ed1c8353a4590e
Miguel <miguelgao@hotmail.com>
2018-05-16 04:46:35 -0500

    estructura login creada

---
+++ app/src/main/java/co/edu/uniquindio/android/electiva/proyecto/activity/Login/AdmBienvenidoActivity.kt
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.android.electiva.proyecto.activity.Login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import co.edu.uniquindio.android.electiva.proyecto.R
+
+class AdmBienvenidoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bienvenido)
+    }
+}
---
+++ app/src/main/java/co/edu/uniquindio/android/electiva/proyecto/activity/Login/CliBienvenidoActivity.kt
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.android.electiva.proyecto.activity.Login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import co.edu.uniquindio.android.electiva.proyecto.R
+
+class CliBienvenidoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cli_bienvenido)
+    }
+}
